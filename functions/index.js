e2fd20568659eb94b0d69eb62e9424bed9449708
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-13 23:16:14 +0545

    Add enpoint request http trigger

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// http request 1 : endpoint request 
+exports.randomNumber = functions.https.onRequest((request, response)=>{
+    const number = Math.round(Math.random() * 100);
+    // log the number in ''functions logs'
+    console.log(number);
+    response.send(number.toString());
+});
+
+// http request 2 : endpoint request 
+exports.redirectToGitHub = functions.https.onRequest((request, response)=>{
+    response.redirect('https://github.com/viveshCodes');
+});
