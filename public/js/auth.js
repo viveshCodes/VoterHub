50151cedc1b67e6bffce34165f665801848a8c1d
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-14 11:16:27 +0545

    sign up user

--- public/js/auth.js
+++ public/js/auth.js
@@ -8,3 +8,26 @@ authSwitchLinks.forEach(link => {
     authModals.forEach(modal => modal.classList.toggle('active'));
   });
 });
+
+
+/*______________Signup, Login, Signout_________________ */
+const registerForm = document.querySelector('.register');
+const loginForm = document.querySelector('.login');
+const signOut = document.querySelector('.sign-out');
+
+// register
+registerForm.addEventListener('submit', (event) =>{
+    event.preventDefault();
+
+    const email = registerForm.email.value.trim();
+    const password = registerForm.password.value.trim();
+    
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(cred =>{
+            console.log(cred.user);
+
+            registerForm.reset();
+        }).catch(err =>{
+            registerForm.querySelector('.error').textContent = error.message;
+        })
+});
