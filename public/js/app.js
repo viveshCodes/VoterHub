30a3aa43e18b7dadf7d3e9ddc12cc990f5f30655
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-14 18:26:50 +0545

    Add request from client side

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 const requestModal = document.querySelector('.new-request');
 const requestLink = document.querySelector('.add-request');
-
+const requestForm = document.querySelector('.new-request form');
 // open request modal
 requestLink.addEventListener('click', () => {
   requestModal.classList.add('open');
@@ -11,5 +11,24 @@ requestModal.addEventListener('click', (e) => {
   if (e.target.classList.contains('new-request')) {
     requestModal.classList.remove('open');
   }
+});
+
+
+// add request to db via functions
+requestForm.addEventListener('submit', (event) =>{
+  event.preventDefault();
+
+  // let's invoke our cloud functions
+  const addRequest = firebase.functions().httpsCallable('addRequest');
+  addRequest({    // invoke above const
+    text: requestForm.request.value.trim()
+  }).then(()=>{
+    requestForm.reset();
+    requestModal.classList.remove('open');
+    requestForm.querySelector('.error').textContent = '';
+
+  }).catch(err =>{
+    requestForm.querySelector('.error').textContent = err.message;
+  })
 });
 
