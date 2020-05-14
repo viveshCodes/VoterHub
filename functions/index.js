95331cd839c45578d3e7671a5e4f4abb3e457920
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-14 16:44:55 +0545

    Create user records

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,19 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
+admin.initializeApp();
 // auth trigger (new user signup)
 exports.newUserSignup = functions.auth.user().onCreate(user =>{
-    console.log('user created', user.email, user.uid);
+    // for background triggers we must return a value/promise
+   return admin.firestore().collection('users').doc(user.uid)        // here user is new user created
+        .set({
+            email:user.email,
+            upvotedOn :[]
+        });
 });
 
 // auth trigger (user delete)
 exports.userDeleted = functions.auth.user().onDelete(user =>{
-    console.log('user deleted', user.email, user.uid);
+    const doc = admin.firestore().collection('users').doc(user.uid);       // here user is new user created
+        return doc.delete();
+       
 });
